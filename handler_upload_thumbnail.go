dda14f86a1bbe8b51c0abbf9e413dbc27a5fb44c
jacobmichaellemon <jacobmichaellemon@gmail.com>
2026-05-30 16:17:05 -0700

    implemented upload thumbnail handler

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +32,56 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory int64 = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mType := header.Header.Get("Content-Type")
+	mimeType, _, err := mime.ParseMediaType(mType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to parse media type", err)
+		return
+	}
+	extensions, err := mime.ExtensionsByType(mimeType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to find extension based on file type", err)
+		return
+	}
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "not users video", err)
+		return
+	}
+
+	fileStorageLocation := filepath.Join(cfg.assetsRoot, videoIDString)
+	fileStorageLocation = fileStorageLocation + extensions[0]
+	thumbnailFile, err := os.Create(fileStorageLocation)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to create file at location", err)
+		return
+	}
+	_, err = io.Copy(thumbnailFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error writing thumbnail to file", err)
+		return
+	}
+
+	root := fmt.Sprintf("http://localhost:%s/", cfg.port)
+	thumbnailUrl := root + fileStorageLocation
+	video.ThumbnailURL = &thumbnailUrl
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error updating video thumbnail", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
